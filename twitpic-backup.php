3b4d1af4c8018404c7d263a3889d492753567839
muphli <hello@muph.li>
2012-07-07 01:14:58 +0200

    Fixed: missing argument username

--- twitpic-backup.php
+++ twitpic-backup.php
@@ -32,65 +32,71 @@
 	$imageCounter = $response->photo_count;
 	$pageCounter = ceil(($imageCounter)/20);
 	
-	if (isset($foldername)){
-		$path = getcwd() . '/' . $foldername;
-	}
-	else $path = getcwd() . '/' . 'twitpic-backup_' . $username;
+	if (isset($username)){
 		
-	//delete old folder if exists
-	if (is_dir(getcwd() . '/' .  $foldername)){
-			$filesInDir = glob($path . '/*');
-			foreach ($filesInDir as $file){
-				unlink($file);
-			}
-		rmdir($path);
-	}
-	mkdir($path);
-	
-	//print total number of images
-	echo "\n@" . $username . " uploaded " . $imageCounter. " images to TwitPic.\n";
-	echo "\nDownload initiated...\n";
-	
-	//for all pages...
-	for ($i = 1; $i <= $pageCounter; $i++){
-	  
-  		//read xml
-  		$response = file_get_contents("http://api.twitpic.com/2/users/show.json?username=" . $username . "&page=" . $i);
-    	$page = json_decode($response);
-    	    
-    	//for all images of one page...
-    	for ($j = 0; $j < count($page->images); $j++){
-    	    	    	
-    		//read short-id and timestamp
-    		$imageID = $page->images[$j]->short_id;
-      		$imageTimestamp = $page->images[$j]->timestamp;
-      		$imageType = $page->images[$j]->type;
-      		
-      		//set url
-      		$imageUrl = "http://twitpic.com/show/full/" . $imageID;
-      		
-      		//set filename
-      		$filename = $imageTimestamp . "." . $imageType;
-      		
-      		//download image
-      		file_put_contents($path . "/" . $filename, file_get_contents($imageUrl));
-      		
-      		//decrement counter
-      		$imageCounter--;
-      		
-      		//print status message for each image
-      		echo "Download of image with ID '" . $imageID . "' successful (" . $imageCounter . " images left).\n";
-    	
-    	}
-    	
-    	if ($imageCounter == 0){
-    	
-    		//print status message if download is complete
-	    	echo "Download complete.";
+		if (isset($foldername)){
+		$path = getcwd() . '/' . $foldername;
+		}
+		else $path = getcwd() . '/' . 'twitpic-backup_' . $username;
+			
+		//delete old folder if exists
+		if (is_dir(getcwd() . '/' .  $foldername)){
+				$filesInDir = glob($path . '/*');
+				foreach ($filesInDir as $file){
+					unlink($file);
+				}
+			rmdir($path);
+		}
+		mkdir($path);
+		
+		//print total number of images
+		echo "\n@" . $username . " uploaded " . $imageCounter. " images to TwitPic.\n";
+		echo "\nDownload initiated...\n";
+		
+		//for all pages...
+		for ($i = 1; $i <= $pageCounter; $i++){
+		  
+	  		//read xml
+	  		$response = file_get_contents("http://api.twitpic.com/2/users/show.json?username=" . $username . "&page=" . $i);
+	    	$page = json_decode($response);
+	    	    
+	    	//for all images of one page...
+	    	for ($j = 0; $j < count($page->images); $j++){
+	    	    	    	
+	    		//read short-id and timestamp
+	    		$imageID = $page->images[$j]->short_id;
+	      		$imageTimestamp = $page->images[$j]->timestamp;
+	      		$imageType = $page->images[$j]->type;
+	      		
+	      		//set url
+	      		$imageUrl = "http://twitpic.com/show/full/" . $imageID;
+	      		
+	      		//set filename
+	      		$filename = $imageTimestamp . "." . $imageType;
+	      		
+	      		//download image
+	      		file_put_contents($path . "/" . $filename, file_get_contents($imageUrl));
+	      		
+	      		//decrement counter
+	      		$imageCounter--;
+	      		
+	      		//print status message for each image
+	      		echo "Download of image with ID '" . $imageID . "' successful (" . $imageCounter . " images left).\n";
 	    	
-    	}
-  
+	    	}
+	    	
+	    	if ($imageCounter == 0){
+	    	
+	    		//print status message if download is complete
+		    	echo "Download complete.";
+		    	
+	    	}
+			
+		}
+	
   	}
+  	
+  	else echo "Argument 'username' is missing. Please call this file as follows: php twitpic-backup.php username [destination-folder]."
   	
 ?> 
 	
